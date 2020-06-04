93a3fb63c8ba9849d9c7d141f2c694a437d90eb0
Alvaro Treviño <artrevinho@gmail.com>
2020-06-04 14:01:24 +0200

    Initial commit

---
+++ VS/SharpNoiseEnums/SharpNoiseEnums/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpNoiseEnums
+{
+    public class SNEnums
+    {
+        public enum ImageFormat { Bmp, Jpeg, Png, Tiff };
+
+
+        public enum DownSampling { Off, Twice, Quarter, Eight, Sixteen };
+
+        
+    }
+}
